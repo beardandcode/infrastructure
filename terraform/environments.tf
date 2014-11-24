39a2159e2b3c2e3fea1dfa41eb44f089feaec87a
Tom Booth <tombooth@gmail.com>
2014-11-24 10:55:54 +0000

    Rework DNS and add friends domains

--- terraform/environments.tf
+++ terraform/environments.tf
@@ -43,11 +43,43 @@ resource "dnsimple_record" "root" {
     ttl = 3600
 }
 
-resource "dnsimple_record" "www" {
+resource "dnsimple_record" "wildcard" {
     domain = "${var.domain_name}"
-    name = "www"
+    name = "*"
     value = "${digitalocean_droplet.web.ipv4_address}"
     type = "A"
+    ttl = 3600
+}
+
+resource "dnsimple_record" "fwstringer_root" {
+    domain = "fwstringer.com"
+    name = ""
+    value = "${var.domain_name}"
+    type = "ALIAS"
+    ttl = 3600
+}
+
+resource "dnsimple_record" "fwstringer_www" {
+    domain = "fwstringer.com"
+    name = "www"
+    value = "${var.domain_name}"
+    type = "CNAME"
+    ttl = 3600
+}
+
+resource "dnsimple_record" "alicepayne_root" {
+    domain = "alicepayne.co.uk"
+    name = ""
+    value = "${var.domain_name}"
+    type = "ALIAS"
+    ttl = 3600
+}
+
+resource "dnsimple_record" "alicepayne_www" {
+    domain = "alicepayne.co.uk"
+    name = "www"
+    value = "${var.domain_name}"
+    type = "CNAME"
     ttl = 3600
 }
 
